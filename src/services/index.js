0dd5f3fc22888d89063cc9210c6898716f8aa9ed
David Ervin <dnervin@student.fullsail.edu>
2023-11-30 23:10:44 -0500

    Add axios dependency and create services for fetching cards

---
+++ src/services/index.js
@@ -0,0 +1,32 @@
+// src/services/index.js
+const axios = require('axios');
+
+const externalApiUrl = 'https://api.lorcana-api.com';
+
+async function getAllCards() {
+  try {
+    const response = await axios.get(`${externalApiUrl}/cards/all`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getCardById(set_id) {
+  if (!set_id) {
+    throw new Error('set_id');
+  }
+  try {
+    const response = await axios.get(
+      `${externalApiUrl}/cards/fetch?search=set_id=${set_id}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getAllCards,
+  getCardById,
+};
